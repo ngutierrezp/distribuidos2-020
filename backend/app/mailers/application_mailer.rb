c8638db88941470f192457b44aadeb1cd1aacbac
Nicolas <nicolas.gutierrez.p@usach,cl>
2021-01-07 16:25:02 -0300

    BRANCHE: create a branch for backend

--- backend/app/mailers/application_mailer.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class ApplicationMailer < ActionMailer::Base # rubocop:todo Style/Documentation
-  default from: 'worldgovt.dist@gmail.com'
-  layout 'mailer'
-end
--- backend/config/routes.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do
-  resources :users, only: %I[new create]
-end
